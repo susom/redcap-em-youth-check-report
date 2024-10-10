f108f3a87ac1e089c56c10db981786933c693f9e
ihabz <ihab.zeedia@stanford.edu>
2024-10-10 13:56:29 -0700

    allow all users to see the report link

--- YouthCheckReport.php
+++ YouthCheckReport.php
@@ -4,6 +4,7 @@ namespace Stanford\YouthCheckReport;
 
 require_once "emLoggerTrait.php";
 
+use ExternalModules\ExternalModules;
 class YouthCheckReport extends \ExternalModules\AbstractExternalModule
 {
 
@@ -17,6 +18,28 @@ class YouthCheckReport extends \ExternalModules\AbstractExternalModule
         // Other code to run when object is instantiated
     }
 
+
+    // allow all users to see the report link
+    public function redcap_module_link_check_display($project_id, $link)
+    {
+        if (ExternalModules::isNoAuth()) {
+            // Anyone can view NOAUTH pages.
+            // Remember, redcap_module_link_check_display() is currently only called for pages defined in config.json.
+            return $link;
+        }
+
+        if ($this->isSuperUser()) {
+            // superusers can see all pages
+            return $link;
+        }
+
+        $username = ExternalModules::getUsername();
+        if (!empty($project_id) && $username) {
+            return $link;
+        }
+
+        return null;
+    }
 
     public function redcap_module_ajax($action, $payload, $project_id, $record, $instrument, $event_id, $repeat_instance, $survey_hash, $response_id, $survey_queue_hash, $page, $page_full, $user_id, $group_id)
     {
